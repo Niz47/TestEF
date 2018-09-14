a3c647b5f4896d80945e824bb41ab8b616bbc5e7
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-09-15 01:39:45 +0630

    add fullname & gender in registration

--- TestEF/Models/AccountViewModels.cs
+++ TestEF/Models/AccountViewModels.cs
@@ -69,6 +69,11 @@ namespace TestEF.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Display(Name = "Full Name"), Required]
+        public string FullName { get; set; }
+
+        public string StrGender { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
