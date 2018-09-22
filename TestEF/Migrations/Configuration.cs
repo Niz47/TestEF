7cb7070c87fbcefc1446f9cc8f4515d6d972d7d2
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-09-22 22:19:38 +0630

    Test

--- TestEF/Migrations/Configuration.cs
+++ TestEF/Migrations/Configuration.cs
@@ -1,8 +1,11 @@
 namespace TestEF.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
     using TestEF.DAL;
     using TestEF.Models;
@@ -17,8 +20,10 @@ namespace TestEF.Migrations
 
         protected override void Seed(TestEF.Models.ApplicationDbContext context)
         {
-            var dbInit = new DbInitializer();
-            dbInit.Seed(context);
+            CreateRolesandUsers(context);
+
+            //var dbInit = new DbInitializer();
+            //dbInit.Seed(context);
 
 
             // context.Student.Add(new Student { Name = "Zin Mar", Age = 26 });
@@ -35,6 +40,70 @@ namespace TestEF.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+
+        // In this method we will create default User roles and Admin user for login   
+        private void CreateRolesandUsers(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            try
+            {
+                // In Startup iam creating first Admin Role and creating a default Admin User    
+                if (!roleManager.RoleExists("Admin"))
+                {
+                    // first we create Admin rool   
+                    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                    role.Name = "Admin";
+                    roleManager.Create(role);
+
+                    //Here we create a Admin super user who will maintain the website                  
+
+                    var user = new ApplicationUser();
+                    user.UserName = "Administrator";
+                    user.Email = "admin@gmail.com";
+
+                    string userPWD = "$Hwe123";
+
+                    var chkUser = UserManager.Create(user, userPWD);
+
+                    //Add default User to Role Admin   
+                    if (chkUser.Succeeded)
+                    {
+                        var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                    }
+                }
+
+                // creating Creating Manager role    
+                if (!roleManager.RoleExists("Manager"))
+                {
+                    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                    role.Name = "Manager";
+                    roleManager.Create(role);
+
+                }
+
+                // creating Creating Employee role    
+                if (!roleManager.RoleExists("Default User"))
+                {
+                    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                    role.Name = "Default User";
+                    roleManager.Create(role);
+
+                }
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+            }
         }
     }
 }
---
+++ TestEF/Models/Anime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestEF.Models
+{
+    public class Anime
+    {
+        [Key]
+        public int ID { get; set; }
+        public string URL { get; set; }
+        public string IMGURL { get; set; }
+        public string TITLE { get; set; }
+        public string AIRING { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string TYPE { get; set; }
+        public long EPISODES { get; set; }
+        public decimal SCORE { get; set; }
+        public Nullable<System.DateTime> SDATE { get; set; }
+        public Nullable<System.DateTime> EDATE { get; set; }
+    }
+}
